173f3c6ead5b0be58904e71d44dcfc67c4d29b3a
ValdimirDenisov <vova_denisov_2003@mail.ru>
2022-05-10 13:36:05 +0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <h1 class="logo">
+      Todo<span class="green">List</span>
+    </h1>
+    <div class="block_diviver">
+      <div class="head">Список задач</div>
+      <hr class="diviver">
+      <div class="add_button">+</div>
+    </div>
+    <div class="conteiner">
+      <TodoList v-for="el in els" :key="el" :todo="el"/>
+      <div class="empty_block" v-if="els.length == 0">Добавьте todo, чтобы увидеть список</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from ".//components/todo.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList,
+  },
+  data() {
+    return {
+      els: [
+        {
+          name: 'Название 1',
+          items: [',fjgsijkn;f', 'dkjirjegtjinios', 'sfjdijgel']
+        },
+        {
+          name: 'Название 2',
+          items: [',fjgsijkn;f', 'dkjirjegtjinios', 'sfjdijgel']
+        },
+        {
+          name: 'Название 3',
+          items: [',fjgsijkn;f', 'dkjirjegtjinios', 'sfjdijgel']
+        },
+        {
+          name: 'Название 4',
+          items: [',fjgsijkn;f', 'dkjirjegtjinios', 'sfjdijgel']
+        },
+        {
+          name: 'Название 5',
+          items: [',fjgsijkn;f', 'dkjirjegtjinios', 'sfjdijgel']
+        },
+        {
+          name: 'Название 6',
+          items: [',fjgsijkn;f', 'dkjirjegtjinios', 'sfjdijgel']
+        },
+      ]
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  .app
+    font-family: 'Open Sans', sans-serif
+    text-align: center
+    .logo
+      font-size: 70px
+      .green
+        color: #51f651
+
+    .block_diviver
+      display: flex
+      justify-content: center
+      align-items: center
+      .head
+        display: inline-block
+        vertical-align: middle
+        font-size: 20px
+        margin-right: 20px
+
+      .diviver
+        width: 70%
+        display: inline-block
+        vertical-align: middle
+        border: 2px dashed #000
+        margin: 0 20px
+        margin-top: 3px
+
+      .add_button
+        display: inline-block
+        vertical-align: middle
+        font-size: 40px
+        cursor: pointer
+
+    .conteiner
+      width: 100%
+      flex-wrap: wrap
+      display: flex
+      justify-content: center
+      align-items: flex-start
+
 </style>
---
+++ src/components/todo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo">
+    <a href="" class="deleter"><img src="../assets/delete.png" alt="" class="deleter_img"></a>
+    <h1>{{ todo.name }}</h1>
+    <ul class="todo_list" >
+      <li v-for="item in todo.items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: ['todo']
+}
+</script>
+
+<style scoped lang="sass">
+  .todo
+    padding: 20px
+    background-color: #e3e3e3
+    width: 300px
+    text-align: center
+    font-family: 'Open Sans', sans-serif
+    margin: 5px
+    cursor: pointer
+    .todo_list
+      text-align: left
+    .deleter
+      text-align: right
+    .deleter_img
+      display: none
+      width: 30px
+
+  .todo:hover
+    .deleter_img
+      display: block
+    h1
+      margin-top: -7px
+
+</style>
